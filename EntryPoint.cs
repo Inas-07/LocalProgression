ecd9f226d2363553e6b24d6b6a0b47715fa6bcbb
Inas-07 <42310741+Inas-07@users.noreply.github.com>
2022-08-18 23:22:48 +0800

    Add project files.

---
+++ EntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+namespace LocalProgression
+{
+    [BepInDependency("dev.gtfomodding.gtfo-api")]
+    [BepInDependency("com.dak.MTFO")]
+    [BepInPlugin("Inas.LocalProgression", "LocalProgression", "0.0.1")]
+    
+    public class EntryPoint: BasePlugin
+    {
+        private Harmony m_Harmony;
+
+        public override void Load()
+        {
+            m_Harmony = new Harmony("LocalProgression");
+            m_Harmony.PatchAll();
+        }
+    }
+}
+
